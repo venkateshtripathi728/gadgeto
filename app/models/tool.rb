68f16a7a9a5dee8052377974cfe99fcf11704ae8
venkatesh tripathi <venkatesh.tripathi@edhec.com>
2021-06-14 17:35:10 +0200

    userform

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,6 +1,6 @@
 class Tool < ApplicationRecord
   belongs_to :user,dependent: :destroy
-
+  has_many_attached :photos
   validates :tool_name, presence: true
   validates :price, presence: true
   validates :tool_description, presence: true,length: {minimum:10}
