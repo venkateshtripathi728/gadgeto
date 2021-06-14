c2c0ffbe904131bfc92d3dd55f7fe552b7d463cf
Maxence Chauvin <maxence.chauvin@edhec.com>
2021-06-14 18:08:27 +0200

    Tools list

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,7 +1,7 @@
 class Tool < ApplicationRecord
-  belongs_to :user,dependent: :destroy
+  belongs_to :user
 
   validates :tool_name, presence: true
   validates :price, presence: true
-  validates :tool_description, presence: true,length: {minimum:10}
+  validates :tool_description, presence: true, length: {minimum:10}
 end
