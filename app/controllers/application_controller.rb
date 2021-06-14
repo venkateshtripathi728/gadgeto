83644ce14fa051c10afec776cf7c831d41705e6e
venkatesh tripathi <venkatesh.tripathi@edhec.com>
2021-06-14 13:10:05 +0200

    user model search devise added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+   
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :description])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:user_name, :description])
+  end
 end
