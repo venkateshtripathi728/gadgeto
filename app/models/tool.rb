fddc0310e1d27280d5b87fc62d51971ca88bdaa2
Gregoire Bazetoux <gregoire.bazetoux@edhec.com>
2021-06-15 16:46:55 +0200

    correction +navbar

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -4,7 +4,6 @@ class Tool < ApplicationRecord
   validates :tool_name, presence: true
   validates :price, presence: true
   validates :category, presence: true
-  validates :photo, presence: true
   validates :tool_description, presence: true,length: {minimum:10}
   has_many :bookings
 end
