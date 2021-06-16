73d629826e5ea523451a75ac1de4a6e196bf5fd2
Thomas Dugue <thomas.dugue@edhec.com>
2021-06-16 12:36:57 +0200

    validation of the category

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,7 +3,8 @@ class Tool < ApplicationRecord
   has_one_attached :photo
   validates :tool_name, presence: true
   validates :price, presence: true
-  validates :category, presence: true
+  validates :category, presence: true, inclusion: { in: %w(Gardening Painting Large\ Works Mechanics Plumbing Electricity),
+            message: "%{value} is not a valid size" }
   validates :address, presence: true
   validates :tool_description, presence: true,length: {minimum:10}
   has_many :bookings
