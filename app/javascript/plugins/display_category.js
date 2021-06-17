8b72bf6aa60911d54debcdbc70bd14612f9eb201
Thomas Dugue <thomas.dugue@edhec.com>
2021-06-17 18:59:57 +0200

    hovering filter

--- app/javascript/plugins/display_category.js
+++ app/javascript/plugins/display_category.js
@@ -1,19 +1,25 @@
 
 const displaycategory = () => {
   const lists = document.querySelectorAll(".category-btn")
-  
+  const arrayLists = Array.from(lists)
   lists.forEach((list) => {
     list.addEventListener('click', (event) => {
-
-      console.log(event.currentTarget)
+      console.log(event.currentTarget.classList.contains("active"))
+      console.log(lists)
       lists.forEach((list) => {
-        list.classList.remove("active")
-      })
-      event.currentTarget.classList.add("active")
+        if (list !== event.currentTarget) {
+          list.classList.remove("active")
+        };
+      });
+      event.currentTarget.classList.toggle("active")
       const tools = document.querySelectorAll(".product-cards")
-      tools.forEach((tool) => {
-        console.log(event.currentTarget.dataset.filter)
-        console.log(tool.dataset.filter)
+      if (!event.currentTarget.classList.contains("active")) {
+        tools.forEach((tool) => {
+          tool.classList.remove('d-none')
+        })
+      }
+      else {
+        tools.forEach((tool) => {
         if (event.currentTarget.dataset.filter != tool.dataset.filter) {
           tool.classList.add('d-none')
         }
@@ -21,9 +27,10 @@ const displaycategory = () => {
           tool.classList.remove('d-none')
         }
       })
+      }
     })
-  
   })
 }
+
 
 export { displaycategory };
