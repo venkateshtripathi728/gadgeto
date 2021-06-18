16081c716b97577ed71b1fdfd90fe065b96e8e6e
venkatesh tripathi <venkatesh.tripathi@edhec.com>
2021-06-18 09:32:31 +0200

    filtercreatednew4

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -10,4 +10,12 @@ class Tool < ApplicationRecord
   has_many :bookings
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+  against: [ :tool_name, :address ],
+  using: {
+      tsearch: { prefix: true }
+    }
+
 end
